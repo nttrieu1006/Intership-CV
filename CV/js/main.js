17206ff3c9df467504facb7571ec70a506e62971
Tan Trieu <ntttrieu1006@gmail.com>
2018-04-20 13:03:46 +0700

    Update Contact JS

--- CV/js/main.js
+++ CV/js/main.js
@@ -1,70 +1,66 @@
-$(document).ready(function(){
-
-});
-
+$(document).ready(function(){});
 var ctx = document.getElementById('scm').getContext('2d');
 var myPieChart = new Chart(ctx,{
-    
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [40, 30, 10 ,20],   
-        }],
-    
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'HTML,CSS,JS',
-            'C#',
-            'Php',
-            'Angularjs'
-        ]
-    },
-    options: {}
+
+type: 'pie',
+data: {
+    datasets: [{
+        data: [40, 30, 10 ,20],   
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'HTML,CSS,JS',
+        'C#',
+        'Php',
+        'Angularjs'
+    ]
+},
+options: {}
 });
 
-var ctx = document.getElementById('knm').getContext('2d');
-var myPieChart = new Chart(ctx,{
-    
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [10, 20, 30],
-        }],
-    
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
-    options: {}
+var ctx2 = document.getElementById('knm').getContext('2d');
+var myPieChart = new Chart(ctx2,{
+
+type: 'pie',
+data: {
+    datasets: [{
+        data: [10, 20, 30],
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+},
+options: {}
 });
 
-function createContact(){
-    var name = $('.contact input[name=name]').val();
-    var email = $('.contact input[name=email]').val();
+function createContact() {
+    var name = $('.contact input[name="name"]').val();
+    var email = $('.contact input[name="email"]').val();
     var message =  $('.contact textarea').val();
-    if(name ==""||email==""){
-        alert("Name and Email are required");
-    }
-    else{
+    if (name == "" || email == "") {
+        window.alert("Name and Email are required !");
+    } else {
         $.ajax({
             url: "http://janeto.us.to:7752/api/contact",
-            method: "post",
-            headers:{ContentType: "application/json"},
-            data:{
-                name:name,
-                email:email,
+            method: "POST",
+            headers: {
+                ContentType: "application/json"
+            },
+            data: {
+                name: name,
+                email: email,
                 message: message
             },
-            success: function(data){
-                alert("OK");
-            },
-            error: function(error){
-                alert("eroor");
+            success: function(data) {
+                alert("Create successfully !");
+                },
+            error: function(err) {
+                alert(err);
             }
         });
-    }
-    
-}
+    }}
